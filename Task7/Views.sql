4db0a3ecafc6e461722d94c4e34edf7578ddd761
SahanaKC1606 <sahanakc1606@gmail.com>
2025-07-03 20:59:24 +0530

    Add files via upload

---
+++ Task7/Views.sql
@@ -0,0 +1,101 @@
+create database company1;
+use company1;
+CREATE TABLE emp (
+    EMPNO INT PRIMARY KEY,
+    ENAME VARCHAR(50),
+    JOB VARCHAR(50),
+    MGR INT,
+    HIREDATE DATE,
+    SAL DECIMAL(10, 2),
+    COMM DECIMAL(10, 2) DEFAULT NULL,
+    DEPTNO INT
+);
+
+INSERT INTO emp (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) VALUES
+(7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800.00, NULL, 20),
+(7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600.00, 300.00, 30),
+(7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250.00, 500.00, 30),
+(7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975.00, NULL, 20),
+(7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250.00, 1400.00, 30),
+(7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850.00, NULL, 30),
+(7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450.00, NULL, 10),
+(7788, 'SCOTT', 'ANALYST', 7566, '1987-04-19', 3000.00, NULL, 20),
+(7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000.00, NULL, 10),
+(7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500.00, 0.00, 30),
+(7876, 'ADAMS', 'CLERK', 7788, '1987-05-23', 1100.00, NULL, 20),
+(7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950.00, NULL, 30),
+(7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000.00, NULL, 20),
+(7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300.00, NULL, 10);
+
+
+CREATE TABLE dept (
+    DEPTNO INT PRIMARY KEY,
+    DNAME VARCHAR(50),
+    LOC VARCHAR(50)
+);
+
+INSERT INTO dept (DEPTNO, DNAME, LOC) VALUES
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
+
+
+
+
+
+CREATE TABLE salgrade (
+    GRADE INT PRIMARY KEY,
+    LOSAL INT,
+    HISAL INT
+);
+
+
+INSERT INTO salgrade (GRADE, LOSAL, HISAL) VALUES
+(1, 700, 1200),
+(2, 1201, 1400),
+(3, 1401, 2000),
+(4, 2001, 3000),
+(5, 3001, 9999);
+
+
+select * from emp;
+
+
+
+select * from dept;
+select * from salgrade;
+
+
+create view empdata as
+select ename,job from emp;
+select ename from empdata where job='clerk';
+
+show index from emp;
+CREATE VIEW dept_avg_salary AS
+SELECT DEPTNO, AVG(SAL) AS avg_salary
+FROM emp
+GROUP BY DEPTNO;
+
+SELECT * FROM dept_avg_salary;
+
+CREATE VIEW old_employees AS
+SELECT EMPNO, ENAME, HIREDATE, JOB, SAL
+FROM emp
+WHERE HIREDATE < '1982-01-01';
+SELECT * FROM old_employees;
+
+CREATE VIEW no_comm_employees AS
+SELECT EMPNO, ENAME, JOB, SAL
+FROM emp
+WHERE COMM IS NULL OR COMM = 0;
+
+select * from no_comm_employees;
+
+
+
+
+
+
+
+
